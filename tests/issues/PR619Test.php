5ddd89af39b9bec72a8f1044f5fbade595b1c8e4
Konrad Abicht <hi@inspirito.de>
2026-06-05 16:21:55 +0200

    Added test to demonstrate fix

---
+++ tests/issues/PR619Test.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use Webklex\PHPIMAP\Client;
+use Webklex\PHPIMAP\Config;
+use Webklex\PHPIMAP\Message;
+use Webklex\PHPIMAP\Connection\Protocols\ImapProtocol;
+
+class PR619Test extends TestCase
+{
+    private function makeClientWithNullActiveFolder(): Client
+    {
+        $config = Config::make([
+            'accounts' => [
+                'default' => [
+                    'host'       => 'localhost',
+                    'protocol'   => 'imap',
+                    'encryption' => 'ssl',
+                    'username'   => 'foo@example.com',
+                    'password'   => 'secret',
+                ],
+            ],
+        ]);
+
+        $client = new Client($config);
+
+        $protocol = $this->createStub(ImapProtocol::class);
+        $protocol->method('connected')->willReturn(true);
+        $client->connection = $protocol;
+
+        // Simulate the state after an implicit reconnect:
+        // Client::connect() always calls disconnect() first,
+        // and disconnect() resets active_folder to null.
+        $ref = new ReflectionClass($client);
+        $prop = $ref->getProperty('active_folder');
+        $prop->setAccessible(true);
+        $prop->setValue($client, null);
+
+        return $client;
+    }
+
+    public function testMakeThrowsTypeErrorWhenActiveFolderIsNull(): void
+    {
+        $this->expectException(\TypeError::class);
+
+        $client = $this->makeClientWithNullActiveFolder();
+        Message::make(1, 0, $client, "Subject: Test\r\n\r\n", '', []);
+    }
+
+    public function testMakeSucceedsAfterFix(): void
+    {
+        $client = $this->makeClientWithNullActiveFolder();
+
+        $message = Message::make(1, 0, $client, "Subject: Test\r\n\r\n", '', []);
+
+        $this->assertInstanceOf(Message::class, $message);
+        $this->assertIsString($message->getFolderPath());
+        $this->assertNotEmpty($message->getFolderPath());
+    }
+}
